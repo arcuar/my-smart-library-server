5e869edbff349e8f02b220f4c98e9739997bb545
happybeen <pvb1226@gmail.com>
2026-05-30 11:49:56 +0900

    cors해결

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
 //초기 진입, 서버, 라우터
 
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 
-const authRoutes = require('./routes/auth');
-const bookRoutes = require('./routes/books');
-const analysisRoutes = require('./routes/analysis');
-const libraryRoutes = require('./routes/library');
+const authRoutes = require("./routes/auth");
+const bookRoutes = require("./routes/books");
+const analysisRoutes = require("./routes/analysis");
+const libraryRoutes = require("./routes/library");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/books', bookRoutes);
-app.use('/api/analysis', analysisRoutes);
-app.use('/api/library', libraryRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/analysis", analysisRoutes);
+app.use("/api/library", libraryRoutes);
 
-app.get('/', (req, res) => res.json({ message: 'My Smart Library API' }));
+app.get("/", (req, res) => res.json({ message: "My Smart Library API" }));
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
